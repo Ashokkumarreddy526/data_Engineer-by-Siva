c5fd14154f621bbaeb1b1119c8bd89869eefe392
Ashokkumarreddy526 <160876807+Ashokkumarreddy526@users.noreply.github.com>
2026-01-30 18:04:10 +0530

    Add files via upload

---
+++ Catalog, Schema creation code.py
@@ -0,0 +1,21 @@
+# Databricks notebook source
+# MAGIC %sql
+# MAGIC CREATE CATALOG IF NOT EXISTS landing_zone;
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC CREATE SCHEMA IF NOT EXISTS landing_zone.vessel;
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC CREATE CATALOG IF NOT EXISTS curated_zone;
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC CREATE SCHEMA IF NOT EXISTS curated_zone.vessel;
+
+# COMMAND ----------
+
